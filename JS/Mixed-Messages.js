2d92bd278962b861b569319c4a1be348b40696e0
“William <williamwildernash@gmail.com>
2021-10-28 08:57:47 -0400

    Finished Mixed Messages

--- JS/Mixed-Messages.js
+++ JS/Mixed-Messages.js
@@ -13,19 +13,15 @@ const wordThreeResult = words.get("word-3");
 const madLibLocation = document.getElementById("mad-lib");
 const submitButton = document.getElementById("submit");
 
-//Hides output area at first
-madLibLocation.hidden = true;
-
-
-//Turns HTML form inputs into sentence arrays
+//Turns URL variables into sentence arrays
 const nounSentence = [`The ${wordOneResult} jumped over the fence.`,`The dog jumped over the ${wordOneResult}.`,
-`Everyone loves ${wordOneResult}.`, `I need ${wordOneResult} in my life.`];
+`Why can't the world be more like a ${wordOneResult}.`, `I need a ${wordOneResult} in my life.`];
 const adjectiveSentence = [`I don't like ${wordTwoResult} dogs.`, `I like ${wordTwoResult} dogs a little too much.`,
 `Have you ever seen a cloud look like a ${wordTwoResult} whale?`, `I need my ${wordTwoResult} back rubbed.`];
 const verbSentence = [`I better ${wordThreeResult} over to the post office.`,`Don't mind me, I'm going to go for a ${wordThreeResult}.`,
 `Should I ${wordThreeResult} this or no?`,`I want you to ${wordThreeResult} me.`];
 
-//Generates random numbers to be ussed on sentence arrays above
+//Generates random numbers to be ussed on sentence arrays
 const randomNumGenerator1 = (Math.floor(Math.random()*4));
 const randomNumGenerator2 = (Math.floor(Math.random()*4));
 const randomNumGenerator3 = (Math.floor(Math.random()*4));
@@ -33,10 +29,5 @@ const randomNumGenerator3 = (Math.floor(Math.random()*4));
 //Generates random Mad Lib by choosing one sentence from each sentence array
 let madLibOutput = `${nounSentence[randomNumGenerator1]} ${adjectiveSentence[randomNumGenerator2]} ${verbSentence[randomNumGenerator3]}`;
 
-//Reveals output area upon clicking submit
-submitButton.onclick = function(){
-    madLibLocation.innerHTML = madLibOutput;
-    madLibLocation.hidden = false;
-}
-
-
+//Places random Mad Lib on webpage
+madLibLocation.innerHTML = madLibOutput;
