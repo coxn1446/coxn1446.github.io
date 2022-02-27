494eee1037847294bea1dbe71f8435ebd52f2ef7
“William <williamwildernash@gmail.com>
2022-02-27 12:10:53 -0500

    jamming nav points to coxnswebsite

--- jamming/src/Components/Navigation/navigation.js
+++ jamming/src/Components/Navigation/navigation.js
@@ -9,10 +9,7 @@ class Navigation extends React.Component {
             <div>
                 <h1>Ja<span className="highlight">mmm</span>ing </h1>
                 <div class="itemABContainer" id="itemABContainer">
-                    <div class="itemABA" id="itemABA"><a href="https://coxn1446.github.io/index.html">Home</a></div>
-                    <div class="itemABB" id="itemABB"><a href="https://coxn1446.github.io/HTML/About.html">About</a></div>
-                    <div class="itemABC" id= "itemABC"><a href="https://coxn1446.github.io/HTML/Projects.html">Projects</a></div>
-                    <div class="itemABD" id= "itemABD"><a href="https://coxn1446.github.io/HTML/ContactMe.html">Contact</a></div>
+                    <div class="itemABA" id="itemABA"><a href="https://coxnswebsite.surge.sh/">Back to Coxn's Webiste</a></div>
                 </div>
             </div>
         )
